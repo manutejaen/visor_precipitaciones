9a3fb8b4e6012652d2a9c96396d966c0ecc9dba4
manutejaen <manuelmartinezmarin3m@gmail.com>
2016-09-27 11:28:02 +0200

    Proper renders applied

--- js/app.js
+++ js/app.js
@@ -3,12 +3,75 @@
 require([
   "esri/Map",
   "esri/views/SceneView",
+  "esri/layers/FeatureLayer",
+
+  "esri/renderers/SimpleRenderer",
+  "esri/symbols/SimpleFillSymbol",
+  "esri/symbols/PolygonSymbol3D",
+  "esri/symbols/ExtrudeSymbol3DLayer",
+
   "dojo/domReady!"
-], function(Map, SceneView) {
+], 
+function(Map, 
+        SceneView, FeatureLayer,
+        SimpleRenderer, SimpleFillSymbol, PolygonSymbol3D, ExtrudeSymbol3DLayer
+        ){
+
+
+/*
+  var defaultSym = new SimpleFillSymbol({
+      outline: {
+        color: "lightgray",
+        width: 0.1
+      }
+  });
+*/
+
+
+
+
+
+  var renderer = new SimpleRenderer({
+    symbol: new PolygonSymbol3D({
+      symbolLayers: [new ExtrudeSymbol3DLayer()]
+    }),
+    label: "Precipitaciones mensuales en mm",
+    visualVariables: [{
+      type: "size",
+      field:"ene",
+      valueUnit: "kilometers"
+    },{
+      type: "color",
+      field: "ene",
+      maxDataValue: 100,
+      minDataValue: 0,
+      colors: ["#FFFCD4","#350242"]
+    }]
+  });
+
+  var precipitaciones_lyr = new FeatureLayer({
+    url: "http://services6.arcgis.com/IKNzV87WiinOV4AF/arcgis/rest/services/Precipitaciones_provincia_mes/FeatureServer/0",
+    renderer: renderer,
+    outFields: ["*"],
+    popupaTemplate:  {
+          title: "{Texto}, {CCAA}",
+          content: "Prueba",
+          fieldInfos: [
+          {
+            fieldName: "sep",
+            format: {
+              digitSeparator: false,
+              places: 0
+            }
+          }]
+        }
+  });
+
   // Code to create the map and view will go here
   var map = new Map({
     basemap: "dark-gray",
-    ground: "world-elevation"
+    ground: "world-elevation",
+    layers: [precipitaciones_lyr]
   });//map
 
   var view = SceneView({
