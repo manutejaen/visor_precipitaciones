567304e463a3ba58caa9d2728acc4d30a552e075
manutejaen <manuelmartinezmarin3m@gmail.com>
2016-10-14 17:53:05 +0200

    Some CSS and Panels

--- js/app.js
+++ js/app.js
@@ -4,7 +4,9 @@ require([
   "esri/Map",
   "esri/views/SceneView",
   "esri/layers/FeatureLayer",
+  "esri/layers/GraphicsLayer",
 
+  "esri/Graphic",
   "esri/renderers/SimpleRenderer",
   "esri/symbols/SimpleFillSymbol",
   "esri/symbols/PolygonSymbol3D",
@@ -24,8 +26,8 @@ require([
   "dojo/domReady!"
 ], 
 function(Map, 
-        SceneView, FeatureLayer, 
-        SimpleRenderer, SimpleFillSymbol, PolygonSymbol3D, ExtrudeSymbol3DLayer,
+        SceneView, FeatureLayer, GraphicsLayer,
+        Graphic, SimpleRenderer, SimpleFillSymbol, PolygonSymbol3D, ExtrudeSymbol3DLayer,
         Search
         ){
 
@@ -38,10 +40,6 @@ function(Map,
       }
   });
 */
-
-
-
-
 
   var renderer = new SimpleRenderer({
     symbol: new PolygonSymbol3D({
@@ -80,14 +78,14 @@ function(Map,
         }
   });
 
-
+  var graphicsLayer = new GraphicsLayer();
 
   //Map and Scene
   // Code to create the map and view will go here
   var map = new Map({
     basemap: "dark-gray",
     ground: "world-elevation",
-    layers: [precipitaciones_lyr]
+    layers: [precipitaciones_lyr, graphicsLayer]
   });//map
 
   var view = new SceneView({
@@ -98,6 +96,41 @@ function(Map,
     padding: {top: 50}
   });
 
+  //Map and view events
+  // Set up a click event handler and retrieve the screen point
+  view.on("click", function(evt) {
+    var screenPoint = evt.screenPoint;
+
+    // the hitTest() checks to see if any graphics in the view
+    // intersect the given screen point
+    view.hitTest(screenPoint)
+      .then(getGraphics);
+  });
+
+  function getGraphics(response) {
+    // the topmost graphic from the click location
+    // and display select attribute values from the
+    // graphic to the user
+    var graphic = response.results[0].graphic;
+    var attributes = graphic.attributes;
+    var provincia = attributes.Texto;
+    var month = attributes.ene;
+
+    // symbolize all line segments with the given
+    // storm name with the same symbol
+    var renderer = new UniqueValueRenderer({
+      field: "NAME",
+      defaultSymbol: layer.renderer.symbol || layer.renderer.defaultSymbol,
+      uniqueValueInfos: [{
+        value: name,
+        symbol: new SimpleLineSymbol({
+          color: "orange",
+          width: 5
+        })
+      }]
+    });
+    layer.renderer = renderer;
+  }
 
   //Search Widget
 
@@ -108,9 +141,38 @@ function(Map,
   searchWidget.startup();
 
 
+  
   //Charts
-
   var chart = new Cedar({
+    type:"bar-horizontal",
+    dataset: {
+      url:"http://services6.arcgis.com/IKNzV87WiinOV4AF/arcgis/rest/services/Precipitaciones_provincia_mes/FeatureServer/0",
+      query: {
+        orderByFields: "ene DESC"
+      },
+      mappings: {
+        "x": {"field":"ene","label":"Precipitaciones (mm)"},
+        "y": {"field":"Texto","label":""}
+      }
+    } //dataset
+  });
+
+
+  // Text mouse over chart
+  chart.tooltip = {
+    "title": "{Texto}",
+    "content": "{ene} mm en el mes de Enero"
+  }
+
+  chart.show({
+    elementId: "#barChart",
+    width:300,
+    height:600
+  }); //chart
+
+
+
+  var chart2 = new Cedar({
     type:"bar",
     dataset: {
       url:"http://services6.arcgis.com/IKNzV87WiinOV4AF/arcgis/rest/services/Precipitaciones_provincia_mes/FeatureServer/0",
@@ -118,13 +180,23 @@ function(Map,
         orderByFields: "ene DESC"
       },
       mappings: {
-        "x": {"field":"Texto","label":""},
+        "x": {"field":"Texto","label":"provincia"},
         "y": {"field":"ene","label":"Precipitaciones (mm)"}
       }
     } //dataset
-  }).show({
-    elementId: "#barChart"
+  });
+
+
+  // Text mouse over chart
+  chart2.tooltip = {
+    "title": "{Texto}",
+    "content": "{ene} mm en el mes de Enero"
+  }
+
+  chart2.show({
+    elementId: "#barChart2",
+    height:200
   }); //chart
 
-
 });//require
+
